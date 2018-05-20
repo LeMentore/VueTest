a1e9a385b93ddeeacb16ee4582ce78fac3ff124d
Charlie Frye <klayton.alan@gmail.com>
2018-05-21 00:33:12 +0500

    attack method

--- src/main.js
+++ src/main.js
@@ -16,6 +16,37 @@ new Vue({
       this.gameIsRunning = true
       this.playerHealth = 100
       this.monsterHealth = 100
+    },
+    attack(){
+      let max = 10
+      let min = 3
+      let damage = Math.max(Math.floor(Math.random() * max) + 1, min)
+      this.monsterHealth -= damage
+
+      if(this.monsterHealth <= 0){
+        alert("ПОБЕДА!")
+        this.gameIsRunning = false
+        return
+      }
+
+      max = 12
+      min = 5
+      damage = Math.max(Math.floor(Math.random() * max) + 1, min)
+      this.playerHealth -= damage
+
+      if(this.playerHealth <= 0){
+        alert("ПОРАЖЕНИЕ!")
+        this.gameIsRunning = false
+      }
+    },
+    specialAttack(){
+
+    },
+    heal(){
+
+    },
+    giveUp(){
+
     }
   }
 })
