74f6ec3a1f53ce5c2eeafbce4a455716149f4c2a
Charlie Frye <klayton.alan@gmail.com>
2018-05-20 18:17:04 +0500

    startGame method

--- src/main.js
+++ src/main.js
@@ -2,11 +2,20 @@ import Vue from 'vue'
 import './assets/css/foundation.min.css'
 import './assets/css/app.css'
 
+Vue.config.productionTip = false
+
 new Vue({
   el: '#app',
   data: {
     playerHealth: 100,
     monsterHealth: 100,
     gameIsRunning: false
+  },
+  methods: {
+    startGame(){
+      this.gameIsRunning = true
+      this.playerHealth = 100
+      this.monsterHealth = 100
+    }
   }
 })
